7bcbadd4944e619e22b9e6efaf886540a9c27360
Hawinf <hawinf54@gmail.com>
2024-01-01 06:09:23 +0400

    editing all

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('i got it')
+window.addEventListener('DOMContentLoaded', (event) => {
+    const menu = document.querySelector('.menu');
+    const sidebar = document.querySelector('.menu-sidebar')
+
+    menu.addEventListener('click', () => {
+        sidebar.classList.add('aktif')
+    })
+
+    const close = document.querySelector('.logo-top1')
+    close.addEventListener('click', ()=> {
+        sidebar.classList.remove('aktif')
+    })
+})
