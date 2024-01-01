ece0f9a41768f6811a2e61698ef3eaa2ac3fe222
Hawinf <hawinf54@gmail.com>
2024-01-01 07:35:48 +0400

    adding overlay

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const menu = document.querySelector('.menu');
     const sidebar = document.querySelector('.menu-sidebar')
+    const overlay = document.querySelector('.overlay')
 
     menu.addEventListener('click', () => {
         sidebar.classList.add('aktif')
+        overlay.classList.add('on')
     })
 
     const close = document.querySelector('.logo-top1')
     close.addEventListener('click', ()=> {
         sidebar.classList.remove('aktif')
+        overlay.classList.remove('on')
     })
 })
